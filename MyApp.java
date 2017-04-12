897901e969ad82943f8a3cb213f5a8bcee38cfc5
Oboridav <oboridav@gmail.com>
2017-04-12 22:58:28 +0300

    changeBalance

--- MyApp.java
+++ MyApp.java
@@ -5,30 +5,34 @@ import java.util.Scanner;
 public class main {
 	
 	public static int changeBalance(int newValue) {
-		System.out.println("enter balance:");
-
 	   return newValue;
 		}
-	
+		
     public static void main(String[] args) { 
     	Product product = new Product("Edam", 3.3, 120);
     	
     	Scanner input = new Scanner (System.in);
     	
     	int newValue = 0;
+    	System.out.println("enter balance:");
     	if (input.hasNextInt()) {
 			newValue = input.nextInt();
 			}
+    	else {
+    		System.out.println("invalid input");
+    		System.exit(0);
+    	}
+    	
     	product.amount=changeBalance(newValue);
     	System.out.println("Product value is " + product.countValue());
     	product.printProduct();
      }
-
+	
 }
 
  class Product {
 	private String name;
-	private double price;
+	double price;
 	int amount; //Amount in storage
 	public Product(String name, double price, int amount) {
 		this.name = name;
@@ -46,4 +50,3 @@ public class main {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 	}
 }
-
